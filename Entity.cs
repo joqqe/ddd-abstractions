1cb5bf6383628d86f8f04c974f5b5976c36d0137
Jochen Schoonjans <jochen.schoonjans@sdworx.com>
2022-11-15 08:56:22 +0100

    Initial commit

---
+++ Entity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Linq.Expressions;
+
+namespace Domain.Common;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    where TId : notnull
+{
+    public TId Id { get; protected set; }
+
+    public Type ElementType => throw new NotImplementedException();
+
+    public Expression Expression => throw new NotImplementedException();
+
+    public IQueryProvider Provider => throw new NotImplementedException();
+
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity<TId> entity && Id.Equals(entity.Id);
+    }
+
+    public bool Equals(Entity<TId>? other) => Equals((object)other);
+
+    public static bool operator ==(Entity<TId> left, Entity<TId> right) => Equals(left, right);
+    public static bool operator !=(Entity<TId> left, Entity<TId> right) => Equals(left, right);
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
